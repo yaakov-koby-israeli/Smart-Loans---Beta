d0806352e5561eb90bad954cf7e6e0c5e3fc793b
koby <kobyisr1@gmail.com>
2024-11-10 12:45:04 +0200

    Create engine and sessionmaker conections

--- database.py
+++ database.py
@@ -3,4 +3,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 
-SQLALCHEMY_DATABASE_URL=''
+SQLALCHEMY_DATABASE_URL='sqlite:///./smartloans.db'
+
+engine=create_engine(SQLALCHEMY_DATABASE_URL,
+                     connect_args={'check_same_thread':False}
+)
+
+SessionLocal = sessionmaker(autocommit = False, autoflush=False, bind=engine)
+
+Base = sqlalchemy.orm.declarative_base()
