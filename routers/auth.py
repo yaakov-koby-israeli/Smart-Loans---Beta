97b9249e721c4ab3af3629b8f9e00db9dcd8931f
koby <kobyisr1@gmail.com>
2024-11-19 13:26:59 +0200

    Create auth file and create user endpoint

---
+++ routers/auth.py
@@ -0,0 +1,54 @@
+from datetime import timedelta, datetime, timezone
+from http.client import HTTPException
+from typing import Annotated
+from fastapi import HTTPException
+from fastapi import APIRouter, status, Depends, Request
+from pydantic import BaseModel
+from sqlalchemy.orm import Session
+
+from database import SessionLocal
+from models import Users
+from passlib.context import CryptContext
+from fastapi.security import OAuth2PasswordRequestForm , OAuth2PasswordBearer# more secure
+from jose import jwt, JWTError
+from fastapi.templating import Jinja2Templates
+
+
+router = APIRouter(
+    prefix = '/auth',
+    tags=['auth']
+)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+# create user
+class CreateUserRequest(BaseModel):
+    username: str
+    email: str
+    first_name: str
+    last_name: str
+    hashed_password: str
+    role: str
+    public_key: str
+
+
+@router.post ("/create-user", status_code=status.HTTP_201_CREATED)
+async def create_user(db: db_dependency, create_user_request: CreateUserRequest):
+    create_user_model = Users(
+        email=create_user_request.email,
+        username=create_user_request.username,
+        first_name=create_user_request.first_name,
+        last_name=create_user_request.last_name,
+        role=create_user_request.role,
+        public_key = create_user_request.public_key,
+        hashed_password=create_user_request.hashed_password
+    )
+    db.add(create_user_model)
+    db.commit()
