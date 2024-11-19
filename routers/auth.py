86dc963de4a8a96a0b433ec575a7b8846624852c
koby <kobyisr1@gmail.com>
2024-11-19 16:52:59 +0200

    create admin file and readall endpoints

--- routers/auth.py
+++ routers/auth.py
@@ -6,19 +6,22 @@ from fastapi import APIRouter, status, Depends, Request
 from passlib.handlers.bcrypt import bcrypt
 from pydantic import BaseModel
 from sqlalchemy.orm import Session
-
 from database import SessionLocal
 from models import Users
 from passlib.context import CryptContext
-from fastapi.security import OAuth2PasswordRequestForm , OAuth2PasswordBearer# more secure
+from fastapi.security import OAuth2PasswordRequestForm , OAuth2PasswordBearer # more secure
 from jose import jwt, JWTError
-from fastapi.templating import Jinja2Templates
 
 
 router = APIRouter(
     prefix = '/auth',
     tags=['auth']
 )
+
+# to do ->>
+# ->> new secret key idea 2 tansform user data to string and then secret key
+SECRET_KEY = '181c122f90691074df84c617256054dd951b121a51a64591aa35ca9bb99e2c0b'
+ALGORITHM = 'HS256'
 
 def get_db():
     db = SessionLocal()
@@ -29,6 +32,7 @@ def get_db():
 
 db_dependency = Annotated[Session, Depends(get_db)]
 bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated = 'auto')
+oauth2_bearer = OAuth2PasswordBearer(tokenUrl='auth/token')
 
 # create user
 class CreateUserRequest(BaseModel):
@@ -40,8 +44,44 @@ class CreateUserRequest(BaseModel):
     role: str
     public_key: str
 
+class Token(BaseModel):
+    access_token: str
+    token_type: str
 
-@router.post ("/create-user", status_code=status.HTTP_201_CREATED)
+
+def authenticate_user(username: str, password: str, db):
+    user = db.query(Users).filter(Users.username == username).first()
+    if not user:
+        return False
+    if not bcrypt_context.verify(password, user.hashed_password):
+        return False
+    return user
+
+def create_access_token(username: str, user_id: int, role: str,
+                        expires_delta: timedelta):
+    encode = {'sub' : username, 'id': user_id, 'role': role}
+    expires = datetime.now(timezone.utc) + expires_delta
+    encode.update({'exp':expires})
+    return jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_bearer)]):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=ALGORITHM) # --> [ALGORITHM]
+        username: str = payload.get('sub')
+        user_id: int = payload.get('id')
+        user_role: str = payload.get('role')
+        if username is None or user_id is None:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                                detail='Could not validate user.')
+        return {'username': username, 'id': user_id, 'user_role': user_role}
+    except JWTError:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                            detail='Could not validate user.')
+
+
+### Endpoints ###
+
+@router.post ("/", status_code=status.HTTP_201_CREATED)
 async def create_user(db: db_dependency, create_user_request: CreateUserRequest):
     create_user_model = Users(
         email=create_user_request.email,
@@ -54,3 +94,12 @@ async def create_user(db: db_dependency, create_user_request: CreateUserRequest)
     )
     db.add(create_user_model)
     db.commit()
+
+@router.post("/token")
+async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db:db_dependency):
+    user = authenticate_user(form_data.username, form_data.password, db)
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                            detail='Could not validate user.')
+    token = create_access_token(user.username, user.id, user.role, timedelta(minutes=20))
+    return {'access_token': token, 'token_type': 'bearer'}
