5e6811fd536190651ca9305cb14d8f06ca7e4df6
koby <kobyisr1@gmail.com>
2024-11-19 18:26:02 +0200

    create user router

--- main.py
+++ main.py
@@ -1,24 +1,12 @@
 from fastapi import FastAPI
-
-from database import engine, SessionLocal
+from database import engine
 import models
+from routers import auth, admin, users
 
-from routers import auth, admin
-
-# Create an instance of the FastAPI class
 app = FastAPI()
 
 # creating the db -- will run only if db does not exist
 models.Base.metadata.create_all(bind=engine) 
-
-# def get_db():
-#     try:
-#         db = SessionLocal()
-#         yield db
-#     finally:
-#         db.close()
-#
-# db_dependency = Annotated[Session, Depends(get_db)]
 
 # Define the root route
 @app.get("/")
@@ -27,6 +15,7 @@ def root():
 
 app.include_router(auth.router)
 app.include_router(admin.router)
+app.include_router(users.router)
 
 
 
