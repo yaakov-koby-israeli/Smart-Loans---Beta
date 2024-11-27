7b399b5f4eb6823930f9ba83e4b537a2f8ed4384
koby <kobyisr1@gmail.com>
2024-11-27 14:46:19 +0200

    Add transfer-eth end point

--- routers/users.py
+++ routers/users.py
@@ -1,5 +1,5 @@
 from http.client import HTTPException
-from fastapi import Depends, HTTPException, status, APIRouter
+from fastapi import Depends, HTTPException, status, APIRouter, Path
 from pydantic import BaseModel, Field
 from models import Users ,Account ,Loans
 from database import SessionLocal
@@ -16,6 +16,7 @@ router = APIRouter(
 # connect to ganache
 ganache_url = "http://127.0.0.1:7545"
 web3_ganache = Web3(Web3.HTTPProvider(ganache_url))
+
 
 def get_db():
     db = SessionLocal()
@@ -45,12 +46,12 @@ async def ganache_health_check(user: user_dependency, db: db_dependency):
 
     return web3_ganache.is_connected()
 
-
 @router.get("/check", status_code=status.HTTP_200_OK)
 async def health_check(user: user_dependency, db: db_dependency):
     if user is None:
         raise HTTPException(status_code=401, detail='Authenticated Failed')
     return 'user is ok'
+
 
 @router.post("/set-up-account", status_code=status.HTTP_201_CREATED)
 async def set_up_account(user: user_dependency,
@@ -75,3 +76,41 @@ async def set_up_account(user: user_dependency,
     account_set_up_model = Account(**account_set_up.dict(),user_id= user.get("id"))
     db.add(account_set_up_model)
     db.commit()
+
+class TransferRequest(BaseModel):
+    to_account: int = Field (gt = 0, description="Please Add Recipient account ID")
+    amount: float = Field(gt=0,   description="Please Add Amount To Transfer" )
+
+@router.post("/transfer-eth",status_code=status.HTTP_201_CREATED)
+async def transfer_eth(user: user_dependency, db: db_dependency, transfer_request: TransferRequest):
+
+    to_account = transfer_request.to_account
+    amount = transfer_request.amount
+
+    if user is None:
+        raise HTTPException(status_code=401, detail='Authenticated Failed')
+
+    from_account = db.query(Account).filter(Account.user_id == user.get("id")).first()
+    to_account = db.query(Account).filter(Account.account_id == to_account).first()
+
+    if from_account is None or to_account is None:
+        raise HTTPException(status_code=404, detail='Account not found')
+
+    if amount > from_account.balance:
+        raise HTTPException(status_code=400, detail='Insufficient balance')
+
+    user_to_account = db.query(Users).filter(to_account.user_id == Users.id).first()
+
+    # Transfer ETH using Web3
+    tx_hash = web3_ganache.eth.send_transaction(
+        {'from': user.get("public_key"), 'to': user_to_account.public_key, 'value': web3_ganache.to_wei(amount, 'ether')})
+    web3_ganache.eth.wait_for_transaction_receipt(tx_hash)
+    from_account.balance -= amount
+    to_account.balance += amount
+
+    db.add(from_account)
+    db.add(to_account)
+    db.commit()
+
+    return {"message": "ETH transferred successfully", "transaction_hash": tx_hash.hex()}
+
